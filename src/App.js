5aeefe33bd8b1da2df8491e2faaf791303454f14
Esmael Mohammed <113509575+esmaelmoh@users.noreply.github.com>
2022-12-04 22:06:43 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,4 @@
 import './App.css';
-import About from './components/About/About';
-import Navbar from './components/Navbar/Navbar';
-import { Questions } from './components/Questions/Questions';
-import Reviews from './components/Reviews/Reviews';
-import Sector from './components/Sector/Sector';
-import Services from './components/Services/Services';
-import Show from './components/Show/Show';
-import Footer from './components/Footer/Footer'
 import Page from './components/Pages/Page';
 
 function App() {
