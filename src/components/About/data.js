7535973360101fb85443d7691380e8555df370f9
Esmael Mohammed <113509575+esmaelmoh@users.noreply.github.com>
2022-12-04 22:11:06 +0300

    Update data.js

--- src/components/About/data.js
+++ src/components/About/data.js
@@ -1,7 +1,7 @@
 const data =[
     {title:'Services',desc:'We offer you a broad variety of Software Development and IT related services. Our core focus is Outstaffing. We recruit and set up dedicated teams that you manage just as you would full-time employees. We also provide regular Outsourcing services and can assist with Legal Entity Setup for clients who wish to establish a presence in Ukraine ',image:'image'},
     {
-        title:'Who we are',desc:'TopTech is the Software Development Company located in Eastern Europe. Specialize in custom software development on JavaScript, AngularJS, Python, .NET, Web, Design, iOS, Android, QA. We work with our clients to provide technology professionals to help them achieve their goals. We listen to our clients, we understand their needs and do whatever it takes to build a highly productive technical team at a competitive price.',image:'image'
+        title:'Who we are',desc:'Ethio Tech is the Software Development Company located in Ethiopia. Specialize in custom software development on JavaScript, AngularJS, Python, .NET, Web, Design, iOS, Android, QA. We work with our clients to provide technology professionals to help them achieve their goals. We listen to our clients, we understand their needs and do whatever it takes to build a highly productive technical team at a competitive price.',image:'image'
     },
     {title:'Technologies',desc:`We have extensive experience in all aspects of software development and IT including:
 
